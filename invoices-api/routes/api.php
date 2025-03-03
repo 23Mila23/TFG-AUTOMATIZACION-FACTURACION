957e15bea4984b898279f31e4c021290d20e7ea1
Camila Villalba <cami_vp_23@hotmail.com>
2025-03-03 12:55:14 +0100

    authentication sistem implemented

--- invoices-api/routes/api.php
+++ invoices-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClientsController;
 use App\Models\Clients;
 use Illuminate\Http\Request;
@@ -10,3 +11,7 @@ use Illuminate\Support\Facades\Route;
 })->middleware('auth:sanctum'); */
 
 Route::apiResource('clients', ClientsController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
